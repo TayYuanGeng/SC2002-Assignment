618941a5399c27081cbe2cdb004e65db41c09274
Yuan Geng Tay <tayyuangeng@gmail.com>
2024-11-03 22:39:39 +0800

    Shifted files

--- src/interfaces/Outcome.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.time.LocalDate;
-
-public interface Outcome {
-    void setDateOfApp(LocalDate date);
-    void setServiceType(String service);
-    void setConsultationNotes(String notes);
-}
--- src/interfaces/PrescriptionDetails.java
+++
@@ -1,6 +0,0 @@
-package interfaces;
-
-public interface PrescriptionDetails {
-    void setPrescriptionStatus(String status);
-    void setMedications(String[] medicines);
-}
