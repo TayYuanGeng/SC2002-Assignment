fe0b96bc2278fc6ba890c1feb89d8e4744509cdd
Zhe Wei Yeoh <yeoh.zhewei@yahoo.com>
2024-11-08 23:35:01 +0800

    Add DocterController and Modify Docter class.

---
+++ HospitalManagementSystem/src/controllers/DoctorController.java
@@ -0,0 +1,82 @@
+package controllers;
+
+import java.util.Scanner;
+import models.*;
+
+public class DoctorController {
+    public static void main(Account loggedInUser) throws Exception {
+            // You now have access to the logged-in user here
+            Doctor docter = (Doctor)loggedInUser;
+            System.out.println("Welcome, " + loggedInUser.getName());
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("========================================");
+            System.out.println("Welcome to Docter Menu");
+            int choice = 0;
+            do{
+                try {
+                    // Display menu
+                    docter.displayMenu();
+
+                    // Handle Menu choice
+                    choice = sc.nextInt();
+                    sc.nextLine();
+                    switch (choice) {
+                        case 1:
+                            System.out.println("Enter ID of patient: ");
+                            docter.ViewPatientMedicalRecord(sc.nextLine());
+                            break;
+                        case 2:
+                            System.out.println("Enter PatientID: ");
+                            String patientID2 = sc.nextLine();
+                            System.out.println("Enter diagnosis: ");
+                            String diagnosis = sc.nextLine();
+                            System.out.println("Enter treatment: ");
+                            String treatment = sc.nextLine();
+                            docter.UpdateDiagnosisAndTreatment(patientID2, diagnosis, treatment);
+                            break;
+                        case 3:
+                        docter.viewSchedule();
+                            break;
+                        case 4:
+                            System.out.println("Enter Unavailable Date (dd-MM-YYYY HH:MM): ");
+                            docter.setAvailability(sc.nextLine());
+                            break;
+                        case 5:
+                            System.out.println("Enter Appointment Date (dd-MM-YYYY HH:MM): ");
+                            String apptDate = sc.nextLine();
+                            System.out.println("Enter PatientID: ");
+                            String patientID = sc.nextLine();
+                            System.out.println("Type 'y' to accept, 'n' to decline appointment: ");
+                            String response = sc.nextLine();
+                            Boolean responseBool = true;
+                            if (response.equals('n')) {
+                                responseBool = false;
+                            }
+                            docter.acceptDeclineAppt(apptDate, patientID, responseBool);
+                            break;
+                        case 6:
+                            docter.ShowUpcomingAppointments();
+                            break;
+                        case 7:
+                            System.out.println("Enter PatientID: ");
+                            String patientID1 = sc.nextLine();
+                            System.out.println("Enter datetime of completion (dd-MM-YYYY HH:MM): ");
+                            String completionDate = sc.nextLine();
+                            docter.RecordAppointmentOutcome(patientID1, completionDate);
+                            break;
+                        case 8:
+                            
+                            break;
+                        default:
+                            System.out.println("Invalid Input. Please enter an integer (1-5):");
+                            break;
+                    }
+                }
+                catch (Exception e) {
+                System.out.println("Invalid input. Please enter an integer: ");
+                sc.next();
+                }
+            } while(true);
+    }
+}
--- HospitalManagementSystem/src/models/Doctor.java
+++ HospitalManagementSystem/src/models/Doctor.java
@@ -14,82 +14,17 @@ public class Doctor extends Staff {
 
     // Other Methods
     public void displayMenu() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("========================================");
-        System.out.println("Welcome to Docter Menu");
-        int choice = 0;
-        do{
-            try {
-                System.out.println("========================================");
-                System.out.println("(1) View Patient Medical Records");
-                System.out.println("(2) Update Patient Medical Records");
-                System.out.println("(3) View Personal Schedule");
-                System.out.println("(4) Set Availability for Appointments");
-                System.out.println("(5) Accept or Decline Appointment Requests");
-                System.out.println("(6) View Upcoming Appointments");
-                System.out.println("(7) Record Appointment Outcome ");
-                System.out.println("(8) Logout");
+        System.out.println("(1) View Patient Medical Records");
+        System.out.println("(2) Update Patient Medical Records");
+        System.out.println("(3) View Personal Schedule");
+        System.out.println("(4) Set Availability for Appointments");
+        System.out.println("(5) Accept or Decline Appointment Requests");
+        System.out.println("(6) View Upcoming Appointments");
+        System.out.println("(7) Record Appointment Outcome ");
+        System.out.println("(8) Logout");
 
-                System.out.println("========================================");
-                choice = sc.nextInt();
-                sc.nextLine();
-                switch (choice) {
-                    case 1:
-                        System.out.println("Enter ID of patient: ");
-                        ViewPatientMedicalRecord(sc.nextLine());
-                        break;
-                    case 2:
-                        System.out.println("Enter PatientID: ");
-                        String patientID2 = sc.nextLine();
-                        System.out.println("Enter diagnosis: ");
-                        String diagnosis = sc.nextLine();
-                        System.out.println("Enter treatment: ");
-                        String treatment = sc.nextLine();
-                        UpdateDiagnosisAndTreatment(patientID2, diagnosis, treatment);
-                        break;
-                    case 3:
-                        viewSchedule();
-                        break;
-                    case 4:
-                        System.out.println("Enter Unavailable Date (dd-MM-YYYY HH:MM): ");
-                        setAvailability(sc.nextLine());
-                        break;
-                    case 5:
-                        System.out.println("Enter Appointment Date (dd-MM-YYYY HH:MM): ");
-                        String apptDate = sc.nextLine();
-                        System.out.println("Enter PatientID: ");
-                        String patientID = sc.nextLine();
-                        System.out.println("Type 'y' to accept, 'n' to decline appointment: ");
-                        String response = sc.nextLine();
-                        Boolean responseBool = true;
-                        if (response.equals('n')) {
-                            responseBool = false;
-                        }
-                        acceptDeclineAppt(apptDate, patientID, responseBool);
-                        break;
-                    case 6:
-                        ShowUpcomingAppointments();
-                        break;
-                    case 7:
-                        System.out.println("Enter PatientID: ");
-                        String patientID1 = sc.nextLine();
-                        System.out.println("Enter datetime of completion (dd-MM-YYYY HH:MM): ");
-                        String completionDate = sc.nextLine();
-                        RecordAppointmentOutcome(patientID1, completionDate);
-                        break;
-                    case 8:
-                        
-                        break;
-                    default:
-                        System.out.println("Invalid Input. Please enter an integer (1-5):");
-                        break;
-                }
-            }
-            catch (Exception e) {
-            System.out.println("Invalid input. Please enter an integer: ");
-            sc.next();
-            }
-        } while(true);
+        System.out.println("========================================");
     }
 
     public void ViewPatientMedicalRecord(String patientID) {
