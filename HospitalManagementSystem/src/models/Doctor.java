a1b24b75ac33ac58dd366ef87523ac8b68a2ab48
Zhe Wei Yeoh <yeoh.zhewei@yahoo.com>
2024-11-16 17:07:31 +0800

    Add JavaDoc comments for Doctor and Phamarcist classes.

--- HospitalManagementSystem/src/models/Doctor.java
+++ HospitalManagementSystem/src/models/Doctor.java
@@ -1,13 +1,28 @@
 package models;
-
+/**
+ * Doctor class which inherits from Staff class.
+ */
 public class Doctor extends Staff {
+    /**
+     * Default constructor
+     */
     public Doctor(){}
 
+    /**
+     * Constructor
+     *
+     * @param uID The ID of the doctor.
+     * @param name The name of the doctor.
+     * @param pass The password for the doctor's account.
+     * @param r The role assigned to the doctor.
+     */
     public Doctor(String uID,String name, String pass, String r) {
 		super(uID,name,pass,r);
 	}
 
-    // Other Methods
+    /**
+     * Doctor menu.
+     */
     public void displayMenu() {
         System.out.println("========================================");
         System.out.println("(1) View Patient Medical Records");
--- HospitalManagementSystem/src/models/Pharmacist.java
+++ HospitalManagementSystem/src/models/Pharmacist.java
@@ -10,25 +10,51 @@ import controllers.AppointmentController;
 import controllers.CSVUtilsController;
 import interfaces.CSVUtilsInterface;
 
+/**
+ * Pharmacist class which inherits from Staff class.
+ */
 public class Pharmacist extends Staff {
     private ArrayList<Medicine> medicineList = new ArrayList<Medicine>();
     static CSVUtilsInterface csvUtils = new CSVUtilsController();
-
+    
+    /**
+     * Default constructor
+     */
     public Pharmacist(){}
 
+    /**
+     * Constructor
+     *
+     * @param uID The ID of the pharmacist.
+     * @param name The name of the pharmacist.
+     * @param pass The password for the pharmacist's account.
+     * @param r The role assigned to the pharmacist.
+     */
     public Pharmacist(String uID,String name, String pass, String r) {
 		super(uID,name,pass,r);
 	}
 
+    /**
+     * Retrieves medicine list.
+     * 
+     * @return List of medicine available.
+     */
     public ArrayList<Medicine> GetMedicineList() {
         return medicineList;
     }
-
+    
+    /**
+     * Sets medicine list.
+     * 
+     * @param mList List of medicine available.
+     */
     public void SetMedicineList(ArrayList<Medicine> mList) {
         medicineList = mList;
     }
 
-    // Other Methods
+    /**
+     * Pharmacist menu.
+     */
     public void displayMenu() {
         System.out.println("========================================");
         System.out.println("(1) View Appointment Outcome Record ");
